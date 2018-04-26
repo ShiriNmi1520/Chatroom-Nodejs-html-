2e0c4f88c4bcf21de410b481feff677ee1d6b9af
ShiriNmi1520 <s20026352@gmail.com>
2018-04-26 18:43:57 +0800

    9x9挖空

--- ser.js
+++ ser.js
@@ -25,6 +25,38 @@ io.sockets.on('connection', function (socket) {
         socket.broadcast.emit('chat', toClient);
     });
     socket.on('sendchat', function (data) {
+        if (data.message === '1') {
+            socket.emit('chat', {sender: 'Greeting', message: '安安吃飽ㄇ'});
+        } else if (data.message === '2') {
+            socket.emit('chat', {sender: 'Greeting', message: '晚安'});
+        } else if (data.message === '99') {
+            console.log(data.ignore)
+            const one = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            let test = [];
+            let i = [];
+            var result;
+            for (const obj of one) {
+                for (const obj2 of one) {
+                    result = obj * obj2;
+                    if (result == data.ignore){
+                        test.push(" ");
+                    }
+                    else{
+                        test.push(result);
+                    }
+                }
+                i.push(test);
+                test = [];
+            }
+            socket.emit('chat', {sender: '9x9', message: i.join('<br>')});
+        } else if (data.message === 'profile') {
+            socket.emit('chat', {sender: 'class', message: '夜資工一<br>'});
+            socket.emit('chat', {sender: 'name', message: '2410632008 黃御哲<br>'});
+        } else if (data.message === 'clear') {
+            for (i = 0; i <= 20; i++) {
+                socket.emit('chat', {sender: '', message: '<br>'});
+            }
+        }
         console.log("Send here:" + data);
         io.sockets.emit('chat', {sender: socket.username, message: data.message});
     });
