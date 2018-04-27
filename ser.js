4d72f58fec1e5e5f2ebeb01ae12281675f807f6a
ShiriNmi1520 <s20026352@gmail.com>
2018-04-27 14:50:12 +0800

    Midterm(?)

--- ser.js
+++ ser.js
@@ -1,4 +1,4 @@
-var app = require('http').createServer(handler)
+let app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
     , fs = require('fs');
 app.listen(8124, "0.0.0.0");
@@ -17,48 +17,82 @@ function handler(req, res) {
 
 io.sockets.on('connection', function (socket) {
     socket.on('addme', function (jsonMessage) {
+        socket.emit("chat", {sender: "", message: "功能表:<br>1.Who are you?<br>2.九九乘法表" + "<br>3.矩陣表<br>4.猜數字"});
         console.log(JSON.stringify(jsonMessage) + "Login");
         socket.username = jsonMessage.name;
-        var toClient = {sender: "SERVER", message: "歡迎" + jsonMessage.name};
-        socket.emit('chat', toClient);
-        toClient.message = jsonMessage.name + "正在上線";
-        socket.broadcast.emit('chat', toClient);
     });
+
     socket.on('sendchat', function (data) {
-        if (data.message === '1') {
-            socket.emit('chat', {sender: 'Greeting', message: '安安吃飽ㄇ'});
-        } else if (data.message === '2') {
-            socket.emit('chat', {sender: 'Greeting', message: '晚安'});
-        } else if (data.message === '99') {
-            console.log(data.ignore)
-            const one = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-            let test = [];
-            let i = [];
-            var result;
-            for (const obj of one) {
-                for (const obj2 of one) {
-                    result = obj * obj2;
-                    if (result == data.ignore){
-                        test.push(" ");
-                    }
-                    else{
-                        test.push(result);
-                    }
-                }
-                i.push(test);
-                test = [];
+        if (socket.token !== 123 || socket.token === undefined) {
+
+            if (data.message === '矩陣表') {
+                socket.emit("chat", {message: "輸入矩陣N"});
+                socket.token = 123;
             }
-            socket.emit('chat', {sender: '9x9', message: i.join('<br>')});
-        } else if (data.message === 'profile') {
-            socket.emit('chat', {sender: 'class', message: '夜資工一<br>'});
-            socket.emit('chat', {sender: 'name', message: '2410632008 黃御哲<br>'});
-        } else if (data.message === 'clear') {
-            for (i = 0; i <= 20; i++) {
-                socket.emit('chat', {sender: '', message: '<br>'});
+
+            else if (data.message === 'Who are you?') {
+                socket.emit("chat", {
+                    sender: "", message: "==========<br>班級:夜資工一<br>學號" +
+                    ":2410632008<br>姓名:黃御哲<br>=========="
+                });
+                socket.emit("chat", {sender: "", message: "功能表:<br>1.Who are you?<br>2.九九乘法表" + "<br>3.矩陣表<br>4.猜數字"});
+            }
+
+            else if (data.message === "九九乘法表") {
+                const division = 6;
+                let multiplication = [];
+                let result = 0;
+                for (i = 1; i <= 9; i++) {
+                    for (j = 1; j <= 9; j++) {
+                        result = i * j;
+                        if (result == division) {
+                            multiplication.push("$");
+                        }
+                        else multiplication.push(result);
+                    }
+                    socket.emit("chat", {sender: "", message: multiplication});
+                    multiplication = [];
+                }
+                socket.emit("chat", {sender: "", message: "功能表:<br>1.Who are you?<br>2.九九乘法表" + "<br>3.矩陣表<br>4.猜數字"});
+            }
+
+            else {
+                socket.emit("chat", {sender: "", message: "功能表:<br>1.Who are you?<br>2.九九乘法表" + "<br>3.矩陣表<br>4.猜數字"});
+            }
+            console.log("Send here:" + data);
+        }
+
+        else if (socket.token === 123) {
+            if (socket.x === undefined) {
+                let string = "N : " + data.message;
+                socket.emit('chat', {message: string});
+                socket.x = data.message;
+                console.log(socket.token);
+                socket.emit("chat", {message: "輸入整除參數"});
+            }
+
+            else if (socket.x !== undefined && socket.y === undefined) {
+                let string = "整除數:" + data.message;
+                socket.emit('chat', {message: string});
+                socket.y = data.message;
+                console.log(socket.x, socket.y);
+                let array = [];
+                for (i = 1; i <= socket.x; i++) {
+                    for (j = 1; j <= socket.x; j++) {
+                        if (j % socket.y == 0){
+                            array.push("@");
+                        }
+                        else{
+                            array.push(j);
+                        }
+                    }
+                    socket.emit('chat', {sender: "", message: array});
+                    array = [];
+                }
+                socket.token = "";
+                socket.emit("chat", {sender: "", message: "功能表:<br>1.Who are you?<br>2.九九乘法表" + "<br>3.矩陣表<br>4.猜數字"});
             }
         }
-        console.log("Send here:" + data);
-        io.sockets.emit('chat', {sender: socket.username, message: data.message});
     });
 
     socket.on('disconnect', function () {
